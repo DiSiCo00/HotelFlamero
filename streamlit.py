80dc8eccb37c506326a99ea89336a75719e97aa7
DiSiCo00 <147243174+DiSiCo00@users.noreply.github.com>
2023-10-15 20:34:52 +0200

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -30,9 +30,68 @@ nltk.download('vader_lexicon')
 nltk.download('punkt')
 
 with st.sidebar:
-    selected = option_menu('Menu', ['Reserva','Chatbot','Reseñas'])
+    selected = option_menu('Menu', ['Chatbot','Reserva','Reseñas'])
 
 #pd.set_option('mode.chained_assignment', None)
+
+if selected == 'Chatbot':
+    openai.api_type = "azure"
+
+    # Azure OpenAI on your own data is only supported by the 2023-08-01-preview API version
+    openai.api_version = "2023-08-01-preview"
+
+    # Azure OpenAI setup
+    openai.api_base = "https://chatbotflamero.openai.azure.com/" # Add your endpoint here
+    openai.api_key = os.getenv("OPENAI_API_KEY") # Add your OpenAI API key here
+    deployment_id = "chatbot_flamero" # Add your deployment ID here
+    # Azure Cognitive Search setup
+    search_endpoint = "https://chatbotflamero.search.windows.net"; # Add your Azure Cognitive Search endpoint here
+    search_key = Environment.GetEnvironmentVariable("SEARCH_KEY"); # Add your Azure Cognitive Search admin key here
+    search_index_name = "flameroindex"; # Add your Azure Cognitive Search index name here
+    
+    def setup_byod(deployment_id: str) -> None:
+        """Sets up the OpenAI Python SDK to use your own data for the chat endpoint.
+    
+    :param deployment_id: The deployment ID for the model to use with your own data.
+
+    To remove this configuration, simply set openai.requestssession to None.
+    """
+
+        class BringYourOwnDataAdapter(requests.adapters.HTTPAdapter):
+
+            def send(self, request, **kwargs):
+                request.url = f"{openai.api_base}/openai/deployments/{deployment_id}/extensions/chat/completions?api-version={openai.api_version}"
+                return super().send(request, **kwargs)
+
+        session = requests.Session()
+
+        # Mount a custom adapter which will use the extensions endpoint for any call using the given `deployment_id`
+        session.mount(
+            prefix=f"{openai.api_base}/openai/deployments/{deployment_id}",
+            adapter=BringYourOwnDataAdapter()
+    )
+
+        openai.requestssession = session
+
+    setup_byod(deployment_id)
+
+    completion = openai.ChatCompletion.create(
+        messages=[{"role": "user", "content": "What are the differences between Azure Machine Learning and Azure AI services?"}],
+        deployment_id=deployment_id,
+        dataSources=[  # camelCase is intentional, as this is the format the API expects
+        {
+                "type": "AzureCognitiveSearch",
+                "parameters": {
+                    "endpoint": search_endpoint,
+                    "key": search_key,
+                    "indexName": search_index_name,
+                }
+            }
+        ]
+    )
+st.write(completion)
+
+
 if selected == 'Reserva':#st.button('Reserva'):
     st.write("""
     ## REALIZA TU RESERVA AHORA
